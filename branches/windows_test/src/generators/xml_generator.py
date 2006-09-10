92ffd150cd685e3fc11a4eb801f50eda770b9985
anrienord <anrienord@78939a47-381c-0410-b85d-49bf6e4ba744>
2006-09-10 01:54:16 +0000

    Created windows_test branch

---
+++ branches/windows_test/src/generators/xml_generator.py
@@ -0,0 +1,59 @@
+"""
+Copyright (C) Richard Lewis 2006
+
+This software is licensed under the terms of the GNU GPL.
+"""
+
+import pycoon.generators
+from pycoon import apache
+from pycoon.interpolation import interpolate
+from pycoon.components import invokation_syntax
+import lxml.etree
+
+def register_invokation_syntax(server):
+    """
+    Allows the component to register the required XML element syntax for it's invokation
+    in sitemap files with the sitemap_config_parse class.
+    """
+        
+    invk_syn = invokation_syntax()
+    invk_syn.element_name = "generate"
+    invk_syn.allowed_parent_components = ["pipeline", "aggregate"]
+    invk_syn.required_attribs = ["type", "src"]
+    invk_syn.required_attrib_values = {"type": "file"}
+    invk_syn.optional_attribs = []
+    invk_syn.allowed_child_components = []
+
+    server.component_syntaxes[("generate", "file")] = invk_syn
+    return invk_syn
+
+class xml_generator(pycoon.generators.generator):
+    """
+    xml_generator encapsulates an XML source file using the generator interface.
+    """
+
+    def __init__(self, parent, src, root_path=""):
+        """
+        xml_generator constructor.
+
+        @src: the source file path (can be a string to be interpolated
+        upon requests).
+        """
+
+        self.src = src
+        pycoon.generators.generator.__init__(self, parent, root_path)
+        self.description = "xml_generator(\"%s\")" % self.src
+
+    def _descend(self, req, uri_pattern, p_sibling_result=None):
+        return False
+
+    def _result(self, req, uri_pattern, p_sibling_result=None, child_results=[]):
+        """
+        Returns an ElementTree representation of the XML document, interpolating the XML source
+        filename with the given uri_pattern.
+        """
+
+        try:
+            return (True, lxml.etree.parse(open(interpolate(self.context, self.src, uri_pattern, as_filename=True, root_path=self.root_path), "r")).getroot())
+        except:
+            return (False, apache.HTTP_NOT_FOUND)
---
+++ branches/windows_test/src/helpers.py
@@ -0,0 +1,101 @@
+"""
+Copyright (C) Richard Lewis 2006
+
+This software is licensed under the terms of the GNU GPL.
+
+The helpers module contains some general functions which are used in various parts of
+the Pycoon system.
+"""
+
+import re, string, datetime
+from htmlentitydefs import entitydefs
+from pycoon.uri_pattern import uri_pattern
+
+class fake_apache:
+    """
+    This class provides a pretend apache class for when the mod_python module is not available
+    (i.e. command line testing).
+    """
+    def __init__(self):
+        self.HTTP_INTERNAL_SERVER_ERROR = 500
+        self.HTTP_NOT_FOUND = 404
+        self.OK = 200
+        self.DONE = 1000
+        self.SERVER_RETURN = Exception
+    def import_module(self, name):
+        return __import__(name)
+    def server_root(self):
+        return ""
+
+# this compiled regex is used by the strip_amps function
+_strip_amps_regex = re.compile("&(?!(#[0-9]*|" + string.join(entitydefs.keys(), "|") + "))")
+
+def strip_amps(s):
+    """
+    Replaces any ampersands which are not functioning as part of a character entity/reference in the given
+    string with the ampersand character entity.
+    """
+
+    return _strip_amps_regex.sub("&amp;", s)
+
+def correct_script_chars(s):
+    """
+    Replaces any ampersand, less-than and greater-than character entities in the given string with literal &, < and >
+    characters.
+    This function is a bit of a hack to make JavaScript code work properly.
+    """
+    
+    script_start = s.find("<script")
+    if script_start < 0: return s
+    script_end = s.find("</script>")
+
+    script_string = s[script_start:script_end]
+    return s[:script_start] + script_string.replace("&amp;", "&").replace("&lt;", "<").replace("&gt;", ">") + s[script_end:]
+
+# this compiled regex is used by the unescape_url function
+find_hex_codes = re.compile("(%[0-9A-Fa-f]{2})")
+
+def repl_hex_with_char(m):
+    """
+    This is the replacement function for the regex used by unescape_url(). It converts hex numbers encoded
+    in strings to characters.
+    """
+
+    return chr(string.atoi(m.group(0).replace("%",""), 16))
+
+def unescape_url(s):
+    """
+    Converts %hex values in URL strings to literal characters.
+    """
+
+    return find_hex_codes.sub(repl_hex_with_char, s)
+
+class log_buffer:
+    """
+    log_buffer class just implements a write method which writes to standard error
+    with a date stamp and the server name.
+    """
+
+    def __init__(self, iostream, server_name):
+        self.stream = iostream
+        self.server_name = server_name
+    
+    def write(self, data):
+        self.stream.write("[%s]: [\"%s\"] %s\n" % (str(datetime.datetime.today()), self.server_name, data))
+        self.stream.flush()
+
+def attributes2options(attrs):
+    """
+    attributes2options turns the given xml.sax.Attributes into a dictionary. It is used to
+    get initialisation options for generator and transformer components. It returns the value of
+    the "type" attribute and a dictionary of the other attributes.
+    """
+
+    attrs_dict = {}
+    for name, value in attrs.items():
+        attrs_dict[str(name).replace("-", "_")] = str(value)
+
+    if attrs_dict.has_key("type"):
+        return (attrs_dict.pop("type"), attrs_dict)
+    else:
+        return (None, attrs_dict)
